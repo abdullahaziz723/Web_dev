9c9a18b83476637bd6291293a00b708e6be04106
Abdullah <abdullahaziz723@gmail.com>
2025-01-24 10:38:23 +0530

    js

---
+++ JavaScript_concepts/basics.js
@@ -0,0 +1,22 @@
+            //keywords
+            //let const var
+            //hoisting -variable and functions are hoisted  which means ther decleration is moved on the top of the code.
+            //types in Js
+            //primitives and refrences
+
+            //primitives =number ,String  null , undefined,boolean  etc
+            //refrences =[](){}
+            //aise koi bhi value jisko copy krne pr real copy nhi hota blki us main value k refrence pass hojata  h usse hum refrence value khte h ,and jiska copy krne pr real copy hojaye wo primitive value hoti hai
+
+            //object  :-
+            var ghadi={
+                brand:"titan",
+                price:500,
+                color:"black",
+                digital :false
+            }
+            console.log(ghadi);
+
+            console.log(a); // Output: undefined
+var a = 5;
+console.log(a); // Output: 5
