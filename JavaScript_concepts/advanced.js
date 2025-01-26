963f5336c0a91509f70620629c4d7e221373b565
Abdullah <abdullahaziz723@gmail.com>
2025-01-27 00:18:32 +0530

    edit

---
+++ JavaScript_concepts/advanced.js
@@ -0,0 +1,7 @@
+//var const let variables 
+//js has two versions   es5 and es6
+//var was in es5 and const and let are introduced in es6
+//var is function scoped in nature
+//let  and const is braces scoped
+//js m khuch features h jo hum use krte h wo js m nhi h still we can use them as js borrow features from window  and window is a box of features.
+// ex-alert prompt CONSOLE etc
