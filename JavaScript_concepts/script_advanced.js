a584ee451c62d1bf477480a8ced93a115b8e60fe
Abdullah <abdullahaziz723@gmail.com>
2025-03-14 17:39:29 +0530

    more js

---
+++ JavaScript_concepts/script_advanced.js
@@ -0,0 +1,40 @@
+//higher order funcs are functions that take other functions as arguments or return functions as their results
+// //callback functions are functions that are passed as arguments to other functions
+// //closure is a function having access to the parent scope even after the parent function has closed
+//construcor functions
+// //constructor functions are used to create multiple objects of the same type
+//first class functions are functions that can be assigned to a variable passed as an argument or returned from another function
+//new keyword is used to create an instance of a constructor function
+//iife full form is immediately invoked function expression
+//iife is a function that runs as soon as it is defined use to reate a private scope and getter and setter functions are used to access and update private variables
+//getter is a method that gets the property value
+//setter is a method that sets the property value
+// //what is a promise in js?
+// //promise is an object that may produce a single value some time in the future either a resolved value or a reason that it is not resolved
+// //promises are used to handle asynchronous operations in js
+//what is gsap?
+//gsap is a javascript library that helps in animating html elements 
+//prototype is a property that allows you to add new properties to object constructors 
+
+//protutypal inheritance is a way to share methods across multiple objects in js 
+//this keyword refers to the object it belongs to
+//this in global scope is window object
+//this in a method refers to the owner object
+//this in a function refers to the global object
+//this in a constructor refers to the object it creates
+//ek function jo objecct k andar ho usse method kehte hain
+//ek function jo object k bahar ho usse function kehte hain
+//ppure and impure functions 
+
+
+function abcd(){
+    console.log(this)   ;
+}
+abcd();
+abcd();
+
+
+
+
+
+
