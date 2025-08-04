36416ef9f29ed6ca7de8d09cecc15c4607d1aede
abdullah <abdullahaziz723@gmail.com>
2025-08-05 00:29:34 +0530

    props

---
+++ cha_or_React/03_propsTailwind/src/card.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+function Card({username}) {
+  console.log(username);
+  console.log("Card component rendered");
+  // You can use props to customize the card if needed
+  // For example, you can pass an image URL, quote, and author name as props
+  return (
+    <div className="flex flex-col rounded-xl  p-4"
+        style={{
+          border: '0.88px solid',
+  
+          backdropFilter: 'saturate(180%) blur(14px)',
+          background: ' #ffffff0d',
+        }}
+      >
+        <div>
+          <img
+            src="https://res.cloudinary.com/ddcg0rzlo/image/upload/v1652470298/9StaF0UBJfih_df0248.gif"
+            alt="nft-gif"
+            width="400"
+            height="400"
+            className="rounded-xl"
+          />
+        </div>
+        <div className="flex flex-col  rounded-b-xl py-4 ">
+          <div className="flex justify-between">
+            <h1 className="font-RubikBold ">{username}</h1>
+            <h1 className="font-bold font-RubikBold">Price</h1>
+          </div>
+          <div className="flex  justify-between font-mono">
+            <p>#345</p>
+            <p>0.01</p>
+          </div>  
+        </div>
+      </div>
+  );
+}
+
+export default Card;
