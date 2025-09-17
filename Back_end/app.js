9971ab18655abd93f4a9f7b314b3bda52b02ca53
abdullah <abdullahaziz723@gmail.com>
2025-09-17 23:58:53 +0530

    db connect

--- Back_end/app.js
+++ Back_end/app.js
@@ -1,34 +1,38 @@
-const express = require('express');
-const morgan = require('morgan');
-const app =express()
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
 
-app.use(morgan('dev'))
+//database connection
+const dbconnection = require("./config/db");
+//user model
+const userModel = require("./models/user");
+
+app.use(morgan("dev"));
 //middle ware used to extracr data in json format and urlencoded format
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 //to serve static files like css,images
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-app.set('view engine','ejs')
+app.set("view engine", "ejs");
 
+app.use((req, res, next) => {
+  console.log("New Request Made:");
 
-app.use((req,res,next)=>{
-    console.log('New Request Made:');
+  return next();
+});
 
-    return next()
-})
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
-app.get('/',(req,res)=>{
-    res.render('index')
-})
+app.get("/about", (req, res) => {
+  res.send("About Page");
+});
 
-app.get('/about',(req,res)=>{
-    res.send('About Page')
-})
+app.post("/get-form-data", (req, res) => {
+  console.log(req.body);
+  res.send("Form Data Received");
+});
 
-app.post('/get-form-data',(req,res)=>{
-    console.log(req.body)
-    res.send('Form Data Received')
-})
-
-app.listen(5000)
+app.listen(5000);
